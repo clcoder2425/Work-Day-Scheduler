ebc63e7b88bd19ea97208094be4e2e49856f5119
clcoder2425 <141270027+clcoder2425@users.noreply.github.com>
2024-01-03 10:20:17 -0600

    added function to save user entry on click, color timeblock by hour, restart color according to present, past and future

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,51 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+const localDSettings= {};
+dayjs.locale(localDSettings);
+//wait until the browser has finished rendering all the elements(DOM)
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  //Get the current hour of the day using the dayjs library
+  const PresentHour = dayjs().format('H');
+
+  function dataEntry(){
+    $('.saveBtn').on('click', function(){
+      const key = $(this).parent().attr('id');
+      const value = $(this).sibling('.description').val();
+      localStorage.setItem(key, value);
+    });
+
+  }
+
+  function colorByHour(){
+    $('.time-block').each(function(){
+      const blockHour = parseInt(this.id);
+      $(this).toggleClass('past', blockHour < PresentHour);
+      $(this).toggleClass('present', blockHour === PresentHour);
+      $(this).toggleClass('future', blockHour > PresentHour);
+    });
+
+  }
+
+  function restartColor(){
+    $('.time-block').each(function(){
+      const blockHour = parseInt(this.id);
+      if (blockHour == PresentHour){
+        $(this).removeClass('past future').addClass('present');
+      } else if (blockHour < PresentHour){
+        $(this).removeClass('future present').addClass('past');
+        
+      } else{
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+  
+  //Get user entry from local storage and set text area values for each time block.
+  $('.time-block').each(function(){
+    const key = $(this).attr('id');
+    const value = localStorage.getItem(key);
+    $(this).children('.description').val(value);
+  });
+  
 });
